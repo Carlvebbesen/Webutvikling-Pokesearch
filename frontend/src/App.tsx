fad7d22566ff1d49c37a097012e081753239f4c9
andreasrys <andrerys@stud.ntnu.no>
2021-10-11 10:16:45 +0200

    setup App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Redirect, Route, Switch} from "react-router"
+import {BrowserRouter} from 'react-router-dom'
+import OverviewPage from "./pages/Overview";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path={"/"}>
+                        <OverviewPage/>
+                    </Route>
+                    <Route exact path={"/test"}>
+                        <div>
+                            <p>
+                                This is a test
+                            </p>
+                        </div>
+                    </Route>
+                    <Redirect to={"/"}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
