a8cba6b308180cd4a52914e1b8b3f30a936dd43c
mariunsj <mariunsj@stud.ntnu.no>
2021-10-17 14:17:35 +0200

    #3 Added selector for retrieving right pokemon based in ids

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,7 +1,31 @@
 import {atom, selector} from "recoil";
+import {PokemonData} from "../data/pokemonData";
+import {Pokemon} from "../pages/myTeamPage/myTeam";
 
 export const pokemonTeam = atom({
     key: "pokemonTeam",
-    default: []
+    default: [0]
 });
+
+export const updatedPokemonTeam = selector({
+    key: "updatedPokemonTeam",
+    get: ({get}) => {
+        const indexList = get(pokemonTeam);
+
+        const getPokemon = (index : number) => {
+            return PokemonData.find(poke => poke.id === index);
+        };
+
+        const pokemon : Pokemon[] = [];
+
+        for (const index of indexList){
+            const poke : Pokemon | undefined = getPokemon(index)
+            if (poke !== undefined) {
+                pokemon.push(poke)
+            }
+        }
+        return pokemon;
+    }
+
+})
 
