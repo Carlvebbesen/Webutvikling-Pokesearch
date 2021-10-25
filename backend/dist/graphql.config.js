9b000fb907381628bc61802fa1aaa3f1aec97f4a
Henrik Brun Fevang <henribfe@stud.ntnu.no>
2021-10-25 17:35:16 +0200

    Removed dist folder.

--- backend/dist/graphql.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.projects = void 0;
-exports.projects = {
-    app: {
-        schema: ["./data/schema.graphql"],
-        documents: ["**/*.{graphql,js,ts,jsx,tsx}"],
-    }
-};
