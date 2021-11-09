07ea5c83463d0b86c2fe584f1285304fac5ff1c7
Henrik Brun Fevang <henribfe@stud.ntnu.no>
2021-11-09 15:07:31 +0100

    Updated resolvers to be used with both production and test databases

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import { resolvers } from './data/resolvers';
+import { getResolvers } from './data/resolvers';
 import { typeDefs } from './data/schema';
 import { PORT } from './config/config';
+import { Pokemons, Teams } from './db/dbConnector';
 
+const resolvers = getResolvers(Pokemons, Teams);
 const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
 server.applyMiddleware({ app });
